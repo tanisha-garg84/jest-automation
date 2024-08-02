39e4ce6c80dabd518041c0a0ed4dcfccafb8c2aa
Rishi Dutt Shukla <rishidutt.shukla@habilelabs.io>
2024-08-02 13:28:52 +0530

    feat: use imported jest instead of `react-scripts test`

---
+++ internals/testing/jest.unit.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  rootDir: "../../",
+  testPathIgnorePatterns: ["/node_modules/"],
+  setupFilesAfterEnv: ["<rootDir>/src/setupTests.js"],
+  moduleNameMapper: {
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+    "\\.(jpg|jpeg|png|gif|webp|svg)$": "<rootDir>/__mocks__/fileMock.js",
+  },
+  testEnvironment: "jsdom",
+};
